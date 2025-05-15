3428f40d2b39802729517028c9c7302c312d7a7b
adam <adam@softylus.com>
2025-05-15 12:44:04 +0300

    newsletter

---
+++ newsletter-studio/schemaTypes/newsletter.ts
@@ -0,0 +1,22 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'newsletter',
+  title: 'Newsletter',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+      validation: (Rule) => Rule.required().email(),
+    }),
+    defineField({
+      name: 'createdAt',
+      title: 'Created At',
+      type: 'datetime',
+      readOnly: true,
+      initialValue: () => new Date().toISOString(),
+    }),
+  ],
+}) 
